25b49c45ae03569623d08a46c318f4b7cc806e0c
Maxi Kucan <maximilianokucan@gmail.com>
2023-06-22 14:22:57 -0300

    removed wrong character in header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,4 +2,4 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
 	<title>Dictionary App</title>
-</svelte:head>;
+</svelte:head>
